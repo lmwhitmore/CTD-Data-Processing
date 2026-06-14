1649abb53ef81457a130fa5392b28f6c6aed544b
Laura M. Whitmore <lmwhitmore@alaska.edu>
2026-06-14 11:21:20 -0800

    Data Import
    Added NABOS 2025 CTD Data to new folder "Raw". Created new folders "proc" and "output"

--- scripts/CTD_Fluorometer_Calibration.R
+++ scripts/CTD_Fluorometer_Calibration.R
@@ -2,10 +2,12 @@
 ## Original Feb, 24, 2025
 rm(list = ls())
 library(openxlsx)
+library(SimpleMapper)
+
 
 #### Load in Data ####
 #Downcast with corrected oxygen
-downcast = readRDS('./output/2025/downcast_oxcorr.RDS')
+downcast = readRDS('./data/downcast_oxcorr.RDS')
 
 l = which(downcast$v2 > 1)
 downcast$Station[l] ## Station 0, Station 6
@@ -13,7 +15,7 @@ downcast$v2[l] = NA
 downcast$flECO.AFL[l] = NA
 
 #Bottle with corrected oxygen
-bottle = readRDS('./output/2025/bottle_oxcorr.RDS')
+bottle = readRDS('./data/bottle_oxcorr.RDS')
 l = which(bottle$V2 > 1)
 bottle$Station[l] ## Station 0, Station 6
 bottle$V2[l] = NA
@@ -22,12 +24,18 @@ bottle$FlECO.AFL[l] = NA
 plot(y = downcast$depSM, x = downcast$flECO.AFL, ylim = c(4000,0), pch = '.')
 plot(y = downcast$depSM, x = downcast$flECO.AFL, ylim = c(1000,0), pch = '.')
 plot(y = downcast$depSM, x = downcast$flECO.AFL, ylim = c(4000,0), xlim = c(-1,5), pch = '.')
+plot(y = downcast$depSM, x = downcast$v2, ylim = c(4000,0), xlim = c(-1,1), pch = '.')
 
+l = which(downcast$StationNo >0 & downcast$StationNo < 9)
+
+plot(y = downcast$depSM[l], x = downcast$v2[l], ylim = c(500, 0), xlim = c(-0.1,1), pch = '.')
+plot(y = downcast$depSM[l], x = downcast$flECO.AFL[l], ylim = c(500, 0), xlim = c(-1,1), pch = '.')
 
 #Measured Chlorophyll 
 #"G:/Shared drives/NABOS/Data/Hydrochemistry/2023/chlorophyll_with SCN.xlsx"
 #chlorophyll = read.xlsx('./chlorophyll_with SCN.xlsx')
 chlorophyll = read.xlsx('../2023/aHealy 2023 Revised Chlor Final.xlsx', startRow = 3)
+chlorophyll = read.xlsx('./data/Chlorophyll_LW active.xlsx', startRow = 3)
 
 
 #### Match Observational Data ####
@@ -64,12 +72,72 @@ for (stn in unique(chlorophyll$Station)) {
 #Station 21 == great 
 #Station 32 == not great 
 
-sum(diff(chlorophyll$Series) > 1, na.rm = T)
-unique(chlorophyll$Series)
+chlorophyll$datetime = convertToDateTime(x = chlorophyll$Date)
+colorpal = pals::brewer.oranges(n=20)
 
-plot(chlorophyll$Series, pch = '.', cex = 3)
 
-plot(x = chlorophyll$Chl, y = chlorophyll$FluorChl.up, pch = 20)
+plot(x = chlorophyll$Chl, y = chlorophyll$FluorChl.up, 
+     pch = 20, 
+     col = mapColor(x = chlorophyll$Date, pal = colorpal),
+     xlim = c(0,3), ylim = c(0,3))
+
+par(mar = c(4, 4, 1, 4))
+plot(x = chlorophyll$Chl, y = chlorophyll$FluorChl.up, 
+     pch = 20, 
+     col = mapColor(x = chlorophyll$`Fo/Fa`, pal = colorpal,xlim = c(0,5)),
+     xlim = c(0,2.5), ylim = c(0,2.5),
+     xlab = 'Measured Chlorophyll', ylab = 'CTD Sensor, Calculated Chlorophyll')
+SimpleMapper::colorbar(colorpal, zlim = c(0,5))
+mtext(text = 'z = Fo/Fa', side = 2, line = -23)
+
+l = which(chlorophyll$`Fo/Fa` < 2)
+
+trend = lm(chlorophyll$FluorChl.up[l] ~ chlorophyll$Chl[l])
+summary(trend)
+par(mar = c(4, 4, 1, 4))
+plot(x = chlorophyll$Chl[l], y = chlorophyll$FluorChl.up[l], 
+     pch = 20, 
+     xlim = c(0,2.5), ylim = c(0,2.5),
+     xlab = 'Measured Chlorophyll', ylab = 'CTD Sensor, Calculated Chlorophyll')
+abline(a = trend$coefficients)
+mtext(text = 'slope = 1.8, r2 = 0.37', side = 1, line = -2)
+#slope = 1.8
+#intercept = 0.006 
+#r2 = 0.37
+
+par(mar = c(4, 4, 1, 4))
+plot(x = chlorophyll$Phaeo, y = chlorophyll$FluorChl.up, 
+     pch = 20, 
+     col = mapColor(x = chlorophyll$Station, pal = colorpal,xlim = c(0,45)),
+     xlim = c(0,5), ylim = c(0,2.5),
+     xlab = 'Measured Phaeo', ylab = 'CTD Sensor, Calculated Chlorophyll')
+SimpleMapper::colorbar(colorpal, zlim = c(0,45))
+mtext(text = 'z = Station Number', side = 2, line = -23)
+
+colorpal = pals::brewer.qualbin(n=45)
+trend_phaeo = lm(chlorophyll$FluorChl.up[l] ~ chlorophyll$Phaeo[l])
+summary(trend_phaeo)
+
+par(mar = c(4, 4, 1, 4))
+plot(x = chlorophyll$Phaeo[l], y = chlorophyll$FluorChl.up[l], 
+     pch = 20, 
+     xlim = c(0,5), ylim = c(0,2.5),
+     xlab = 'Measured Phaeo', ylab = 'CTD Sensor, Calculated Chlorophyll')
+abline(a = trend_phaeo$coefficients)
+mtext(text = 'slope = 0.27, r2 = 0.45', side = 3, line = -2)
+#slope = 0.27
+#intercept = -0.07 
+#r2 = 0.45
+
+par(mfrow = c(2,1))
+plot(x = chlorophyll$Station, y = as.numeric(chlorophyll$`Chl/Phaeo`), 
+     xlab = 'Station', ylab = "Chl/Phaeo", 
+     pch = 16,
+     ylim = c(-4,4))
+plot(x = chlorophyll$Station, y = 1/as.numeric(chlorophyll$`Chl/Phaeo`), 
+     xlab = 'Station', ylab = "Phaeo/Chl",
+     pch = 16)
+
 plot(x = chlorophyll$Chl, y = chlorophyll$FluorChl.up.V, pch = 20)
 
 plot(x = chlorophyll$Chl, y = chlorophyll$FluorChl.up, pch = 20,
@@ -83,6 +151,9 @@ abline(lm(chlorophyll$FluorChl.up.V ~ chlorophyll$Chl))
 
 plot(x = sort(chlorophyll$Chl, na.last = T), y = sort(chlorophyll$FluorChl.up.V, na.last = T), pch = 20,
      ylim = c(0, 0.2))
+plot(x = sort(chlorophyll$Chl, na.last = T), y = sort(chlorophyll$FluorChl.up, na.last = T), pch = 20,
+     ylim = c(0, 3), xlim = c(0,3))
+
 
 plot(x = downcast$flECO.AFL, y = downcast$depSM, ylim = c(500,0))
 
